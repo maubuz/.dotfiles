de78d1a10eac41eb8b6c6dc3d51d6728842d83ec
Mauricio Buschinelli <mauri.andres@gmail.com>
2024-01-04 17:39:07 -0500

    install packages from txt file

--- fresh-install/workstation-install.sh
+++ fresh-install/workstation-install.sh
@@ -9,22 +9,22 @@ function get-script-path()
 {
     # Var BASH_SOURCE[0] holds the filename of the executing script
     local SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
-
+    
     # Assure path is not zero length and is a file
     if [[ -z "$SCRIPT_PATH" || ! -f "$SCRIPT_PATH" ]]; then
         echo "Error: Cannot resolve the path of the script" >&2
         exit 1
     fi
-
+    
     # Get only directory path to file
     local DIR_PATH=$(dirname "${SCRIPT_PATH}")
-
+    
     # Assure dir path is not zero length is a directory
     if [[ -z "$DIR_PATH" || ! -d "$DIR_PATH" ]]; then
         echo "Error: Cannot resolve the directory of the script" >&2
         exit 1
     fi
-
+    
     echo "$DIR_PATH"
 }
 
@@ -45,12 +45,13 @@ echo "Using package manager $PACKAGE_MANAGER"
 
 # Assure package manager successfully set from `get-package-manager.sh`
 if [ -n "$PACKAGE_MANAGER" ]; then
-
+    
     sudo "$PACKAGE_MANAGER" update -y
-
-    # fd: simple, fast and user-friendly alternative to 'find', writen in rust
-    # https://github.com/sharkdp/fd
-    sudo "$PACKAGE_MANAGER" install fd-find -y
-    echo "See fd-find alias for Ubuntu in 'workstation-aliases'."
-
+    
+    pkg_file="workstation-pkgs-all.txt"
+    
+    read -r -d "\n" -a packages < "${pkg_file}"
+    echo "Installing packages: ${packages[*]}"
+    sudo "$PACKAGE_MANAGER" install "${packages[@]}"
+    
 fi
