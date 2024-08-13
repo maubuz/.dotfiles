8c0814e6b075a207ec2193f197f2b55271b2f514
Mauricio Buschinelli <mauricio.buschinelli@canonical.com>
2024-08-12 23:06:10 -0400

    disable gnome's app-hotkeys

---
+++ fresh-install/gnome-disable-app-hotkeys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Source: [Super+number key combos remapping in GNOME 40 (switch workspaces instead of launch docked apps)](https://unix.stackexchange.com/questions/677878/supernumber-key-combos-remapping-in-gnome-40-switch-workspaces-instead-of-laun)
+
+for i in {1..9}; do 
+  gsettings set "org.gnome.shell.keybindings" "switch-to-application-${i}" "[]"
+  gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-${i}" "['<Super>${i}']"
+  gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-${i}" "['<Super><Shift>${i}']"
+  gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-${i}" "[]"
+done
