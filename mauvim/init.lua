be872310f4cf04d69e65f78df8737fb03ccb6bd8
Mauricio Buschinelli <mauri.andres@gmail.com>
2026-08-15 21:48:57 -0400

    chore: remove old config

--- mauvim/init.lua
+++
@@ -1,7 +0,0 @@
-require('mauvim.keymaps')
-require('mauvim.settings_basic')
-
--- Show special chars such as tab, space. Enable with :set list
-vim.opt.listchars:append({eol = '↲', trail = '·', lead = '·'})
--- Other listchars options
--- vim.opt.listchars= {tab='🡢 ', tab = '▸ ', extends='>', precedes='<', space='·'}
